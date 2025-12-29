e6313c8d2c6ccb81776eb85ed20b518905a03ba9
venusofstar <sumidomikeangelo24@gmail.com>
2025-12-29 22:56:38 +0800

    Update server.js

--- server.js
+++ server.js
@@ -22,7 +22,9 @@ const httpsAgent = new https.Agent({ keepAlive: true, maxSockets: 200, keepAlive
 // =========================
 const ORIGINS = [
   "http://143.44.136.67:6060",
-  "http://136.239.158.18:6610"
+  "http://136.239.158.18:6610",
+  "http://136.158.97.2:6610",
+  "http://136.239.173.10:6610"
 ];
 
 // =========================
